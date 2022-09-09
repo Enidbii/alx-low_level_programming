434474c4fded2a6a94aaa2e22aec3a43358e0390
Enidbii <chebsenid@gmail.com>
2022-09-09 12:03:43 -0700

    6-print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints out single digits of base 10
+ * when executed
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
