d93e9cae526881edcc0791a64ed582464c658c18
Enidbii <chebsenid@gmail.com>
2022-09-14 11:04:53 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,15 +8,11 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	while (n > 98 || n < 98)
 	{
-		_putchar(n + ' ');
-		n++;
-	}
-	else if (n <= 98)
-	{
-		_putchar(n + ' ');
-		n--;
+		if (n == 98)
+			_putchar(98);
+		_putchar((n + 1), ' ');
 	}
 	_putchar('\n');
 }
