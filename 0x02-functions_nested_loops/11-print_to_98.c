90c099fa92df5362de421d7083d233916a6c83ab
Enidbii <chebsenid@gmail.com>
2022-09-14 08:30:40 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - print n to 98 followed by a new line
+ * @n: input integer
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		_putchar(n + ' ');
+		n++;
+	}
+	else if (n <= 98)
+	{
+		_putchar(n + ' ');
+		n--;
+	}
+	_putchar('\n')
+}
