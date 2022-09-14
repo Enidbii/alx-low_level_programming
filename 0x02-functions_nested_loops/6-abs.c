c4e9bdf8f5a9334da91bb3c03c156ed6479906c7
Enidbii <chebsenid@gmail.com>
2022-09-14 00:15:21 -0700

    absolute integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,18 +5,10 @@
  * @r: input integer
  * Return: 0
  */
-int _abs(int)
+int _abs(int r)
 {
-	int r;
-
-	if (r < 0)
-	{
-		r = (-1) * r;
-		_putchar(r);
-	}
+	if (r >= 0)
+		return (r);
 	else
-	{
-		_putchar(r);
-	}
-	return (r);
+		return (r * -1);
 }
