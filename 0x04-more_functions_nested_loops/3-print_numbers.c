017675147dd965ef29c58bf69f4024193fce8fe8
Enidbii <chebsenid@gmail.com>
2022-09-15 07:58:37 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,10 +9,9 @@ void print_numbers(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
+	for (num = '0'; num < '10'; num++)
 	{
 		_putchar(num);
-		_putchar('\n');
 	}
-	return (0);
+	_putchar('\n');
 }
