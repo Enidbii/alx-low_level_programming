4de68502b1c3684701a6392a801e633bd24ba9f7
Enidbii <chebsenid@gmail.com>
2022-09-18 22:55:46 -0700

    reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,11 +3,10 @@
 /**
  * reset_to_98 - takes a pointer and updates the value it
  * points to 98
- * @*n pointer
+ * @n: pointer
  * REturn: 0
  */
 void reset_to_98(int *n)
 {
-int *n = &n;
 *n = 98;
 }
