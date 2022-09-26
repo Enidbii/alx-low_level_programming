4fc7a645cb844de7b8802792105ddca51de53788
Enidbii <chebsenid@gmail.com>
2022-09-26 12:05:15 -0700

    string character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,8 +8,17 @@
  */
 char *_strchr(char *s, char c)
 {
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return(s);
+		}
+		++s;
+	}
 	if (*s == c)
-		return(s);
-	else if (*s = '\0')
-		return(0);
+	{
+		return(s)
+	}
+	return(0);
 }
