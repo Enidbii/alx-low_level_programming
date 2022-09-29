2232459c289da08bcacc9ad5d57e0cf843c6665a
Enidbii <chebsenid@gmail.com>
2022-09-29 02:52:03 -0700

    evaluate

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,13 +7,18 @@
  */
 int _sqrt_recursion(int n)
 {
-	int i;
-
-	if (i == 0 || i == 1)
+	if (n == 0 || n == 1)
 		return (n);
 	else
 		return (evaluate(n, i + 1));
+	return (0);
 }
+/**
+ * evaluate - finds the square root of a number
+ * @n: input integer
+ * @i: suare root of n
+ * Return: i
+ */
 int evaluate(int n, int i)
 {
 	if (n / i == i)
@@ -24,4 +29,5 @@ int evaluate(int n, int i)
 	{
 		return (evaluate(n, i + 1));
 	}
+	return (0);
 }
