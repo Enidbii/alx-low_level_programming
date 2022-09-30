7f820ade11b9faabb4ee9a5fffef6bb3eeae8b89
Enidbii <chebsenid@gmail.com>
2022-09-30 06:20:04 -0700

    args2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ * @argc: no of arguments passed
+ * @argv: arguments passed
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argv[i] > 0)
+	{
+		for (i = 0; i <= argv[i]; i++)
+		{
+			printf("%d\n", argv[i]);
+		}
+	}
+	return (0);
+}
