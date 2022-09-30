461db024e62e498cf27616786cb2b8640696c586
Enidbii <chebsenid@gmail.com>
2022-09-30 01:41:08 -0700

    argv

---
+++ 0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - print the name of the program
+ * @argc: number of arguments entered on the command line
+ * @argv: arguments entered on the command line
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	_putchar(*argv[0]);
+}
