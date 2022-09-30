214d81ccb3b4f4c31ca2fc4d30d4c12d9ac642fa
Enidbii <chebsenid@gmail.com>
2022-09-30 04:49:08 -0700

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints name of the program
+ * @argc: no of arguments while running a program
+ * @argv: the arguments entered when runing a program
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	_putchar(argv[i]);
+	_putchar('\n');
+	return (0);
+}
