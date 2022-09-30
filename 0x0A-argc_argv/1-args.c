d10eb067460d800f1742ebfae52ecdfd6aba152a
Enidbii <chebsenid@gmail.com>
2022-09-30 05:50:37 -0700

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,11 +7,10 @@
  * @argv: arguments passed
  * Return: 0
  */
-int main void(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	(void) argv;
-
-	printf("%i\n", argc);
+	printf("%i\n", argc - 1);
 
 	return (0);
 }
