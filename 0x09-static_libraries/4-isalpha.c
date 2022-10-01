922e6f9c93751d261444967ead3f9b03db986fd6
Enidbii <chebsenid@gmail.com>
2022-10-01 04:33:07 -0700

    libraries

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: input character
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	char low, upp;
+	int ret_urn = 0;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		for (upp = 'A'; upp <= 'Z'; upp++)
+		{
+			if (c == low || c == upp)
+				ret_urn = 1;
+		}
+	}
+	return (ret_urn);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute number
+ * @r: input integer
+ * Return: 0
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
