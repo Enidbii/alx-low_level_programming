a0011d0d5f1a9c59474a349b5e036cce1898cd28
Enidbii <chebsenid@gmail.com>
2022-10-15 03:00:02 -0700

    function pointers

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+
+/**
+ * main - arguments for struct
+ * @argc: number of a arguments
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int k, l;
+	int (*d)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	c = get_op_func(argv[2]);
+	if (c == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	k = atoi(argv[1]);
+	l = atoi(argv[3]);
+
+	printf("%d\n", d(k, l));
+	return (0);
+}
