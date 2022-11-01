b1916e76bfceeb421f028c1c56e7386c1f2390bd
Enidbii <chebsenid@gmail.com>
2022-11-01 11:20:20 -0700

    bitwise

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 	c = 0;
 	for (len = 0; b[len] != '\0'; len++)
 		;
-	for (len--, bin = 1; len >= 0; len--, bin += 2)
+	for (len--, bin = 1; len >= 0; len--, bin *= 2)
 	{
 		if (b[len] != '0' && b[len] != '1')
 			return (0);
