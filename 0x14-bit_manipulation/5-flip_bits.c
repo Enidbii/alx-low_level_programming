d6020c1d98d3a6528d0744625f872a9157bf3e1f
Enidbii <chebsenid@gmail.com>
2022-11-01 11:15:49 -0700

    bitwise

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,7 +11,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int t_b;
 
-	for (t_b = 0; n || m; n >>= 1, m >> = 1)
+	for (t_b = 0; n || m; n >>= 1, m >>= 1)
 	{
 		if ((n & 1) != (m & 1))
 			t_b++;
