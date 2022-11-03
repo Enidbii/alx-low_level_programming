738afba244aefe6826212dfbc891284bf87e76d5
Enidbii <chebsenid@gmail.com>
2022-11-03 04:45:38 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (!text_content)
-		return (-1);
+		text_content = "";
 	for (letters = 0; text_content[letters]; letters++)
 		;
 	lw = write(fd, text_content, letters);
