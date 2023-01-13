d83ce66772584e01e3eac332ca32df833b5fb8c5
Enidbii <chebsenid@gmail.com>
2023-01-13 12:53:08 -0800

    doubly linked lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -42,7 +42,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 						head->next->prev = new;
 						head->next = new;
 					}
-				}break;
+				}
+				break;
 			}
 			head = head->newt;
 			i++;
