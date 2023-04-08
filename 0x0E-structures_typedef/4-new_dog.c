afb5714ab24b282b1a058b257978c6b843921c58
Enidbii <chebsenid@gmail.com>
2023-04-08 04:17:29 -0700

    dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,9 +10,9 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *doggy;
+	dog_t *doggy;
 
-	doggy = malloc(sizeof(struct dog));
+	doggy = malloc(sizeof(dog_t));
 	if (doggy == NULL)
 		return (NULL);
 	doggy->name = name;
