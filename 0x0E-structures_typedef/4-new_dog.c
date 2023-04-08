8c69ab3875385bb06c66396884e5c67a552d0b9b
Enidbii <chebsenid@gmail.com>
2023-04-08 04:47:40 -0700

    dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,8 +13,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *doggy;
 
-	if (name == NULL || owner == NULL)
-		return (NULL);
 	doggy = malloc(sizeof(dog_t));
 	if (doggy == NULL)
 		return (NULL);
@@ -24,20 +22,4 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	return (doggy);
 	free(doggy);
-}
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    dog_t *my_dog;
-    dog_t *this_dog;
-
-    my_dog = new_dog("Poppy", 3.5, "Bob");
-    this_dog = new_dog("Oreo", 0.4, "Leila");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
-    printf("My name is %s, and I am %.1f :) - Woof! and my owner is %s\n", this_dog->name, this_dog->age, this_dog->owner);
-    return (0);
 }
