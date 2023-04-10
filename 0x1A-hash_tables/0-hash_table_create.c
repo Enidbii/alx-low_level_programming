d8d6c1400d25fbd42ecbc54f380069e7440b120a
Enidbii <chebsenid@gmail.com>
2023-04-10 08:32:51 -0700

    create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,14 +8,27 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t* table = (hash_table_t*) malloc(sizeof(hash_table_t));
+	hash_table_t *table = (hash_table_t *) malloc(sizeof(hash_table_t));
 	unsigned long int i;
 
 	table->size = size;
-	table->array = (hash_node_t**) calloc(table->size, sizeof(hash_node_t*));
+	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t *));
 
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
 
-	return table;
+	return (table);
+}
+/**
+ * main - check the code for
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	hash_table_t *ht;
+
+	ht = hash_table_create(1024);
+	printf("%p\n", (void *)ht);
+	return (EXIT_SUCCESS);
 }
