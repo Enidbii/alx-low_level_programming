9a5a2dd35a5288e40f3750b24131a67bcfb0e9e3
Enidbii <chebsenid@gmail.com>
2023-04-10 11:33:17 -0700

    htables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,7 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((const unsigned char *)key, ht->size);
 	for (j = index; ht->array[j]; j++)
 	{
-		if (strtcmp(ht->array[j]->key, key) == 0)
+		if (strcmp(ht->array[j]->key, key) == 0)
 		{
 			free(ht->array[j]->value);
 			ht->array[j]->value = valrec;
