4a2014b18575210cc3b216416f2a7a8bd50a1288
Enidbii <chebsenid@gmail.com>
2023-04-10 11:54:02 -0700

    key index

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * *hash_table_get - retrieves a value associated with a key
+ * @ht: hashtable made
+ * @key: key of the hashtable
+ * Return: value associated with element or NULL if key isnt found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+
+	return ((node == NULL) ? NULL : node->value);
+}
