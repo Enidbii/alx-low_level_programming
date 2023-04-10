d2ee5d650ea74ce9560b09b71ad957b0a25d720a
Enidbii <chebsenid@gmail.com>
2023-04-10 07:29:56 -0700

    tablehash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+#include <stddef.h>
 
 /**
  * *hash_table_create - creates hashtable
@@ -7,13 +8,13 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t table = (hash_table_t *) malloc(sizeof(hash_table_t));
+	hash_table_t table = (hash_table_t*) malloc(sizeof(hash_table_t));
+	int i;
 
 	table->size = size;
-	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t *));
+	table->array = (hash_node_t**) calloc(table->size, sizeof(hash_node_t*));
 
-
-	for (int i = 0; i < table->size; i++)
+	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
 	return (table);
 }
